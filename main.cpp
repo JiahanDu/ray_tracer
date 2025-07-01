a60563a40f479dc19a0de063d6b1d2cd423217b4
Your Name <jiahandu@math.ucla.edu>
2025-06-30 21:53:58 -0700

    m

--- main.cpp
+++ main.cpp
@@ -3,17 +3,24 @@
 #include "vec3.h"
 
 #include <iostream>
-bool hit_sphere(const point3& center, double radius, const ray& r){
+double hit_sphere(const point3& center, double radius, const ray& r){
     vec3 oc=center-r.origin();
     double a=dot(r.direction(),r.direction());
     double b=-2.0*dot(r.direction(),oc);
     double c=dot(oc,oc)-radius*radius;
-    return b*b>=4*a*c;
+    double discriminant=b*b-4*a*c;
+    if(discriminant<0){
+        return -1.0;
+    }else{
+        return (-b+std::sqrt(discriminant))/(2.0*a);
+    }
 }
 
 color ray_color(const ray& r){
-    if(hit_sphere(point3(0,0,1),0.5,r)){
-        return color(1,0,0);
+    double t=hit_sphere(point3(0,0,1),0.5,r);
+    if(t>0.0){
+        vec3 N=normalize(r.at(t)-vec3(0,0,1));
+        return 0.5*color(N.x()+1,N.y()+1,N.z()+1);
     }
     vec3 unit_direction=normalize(r.direction());
     double a=0.5*(unit_direction.y()+1.0);
