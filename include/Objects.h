02828f131a9506fc14484edc1f746ebccd928b0d
Jiahan Du <dujiahan@amazon.com>
2025-07-04 15:02:32 -0700

    m

--- include/Objects.h
+++ include/Objects.h
@@ -4,17 +4,18 @@
 #ifndef OBJECTS_H
 #define OBJECTS_H
 
-class Hit{
+class HitRecord{
   public:
     Point p;
     Point normal;
     double t;
+    bool exiting;
 };
 
 class Objects{
   public:
     virtual ~Objects()= default;
 
-    virtual bool intersect(const Ray& r, double t_min, double t_max, Hit& rec) const=0;
+    virtual bool intersect(const Ray& r, double t_min, double t_max, HitRecord& rec) const=0;
 };
 #endif 
