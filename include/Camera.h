cd9dccdf8c5b13343a454257d5dfac686e93536e
Jiahan Du <dujiahan@amazon.com>
2025-07-05 15:39:31 -0700

    m

--- include/Camera.h
+++ include/Camera.h
@@ -4,16 +4,21 @@
 #define CAMERA_H
 
 class Camera{
-  private:
-    void initialize(){
+  public:
+    int image_width; //Number of pixels per row
+    int image_height; //Number of pixels per column
+    double aspect_ratio=(double)image_width/image_height; 
+    Point center(0,0,0);  //Location of the camera
+    Point top_left; // The top left corner of the view_port
 
+    void initialize(){
+      
     }
     
     Color ray_color(const Ray& r, const Object& world) const{
         
-    }
+    }  
 
-  public:
     void render(const Object& world){
 
     }
