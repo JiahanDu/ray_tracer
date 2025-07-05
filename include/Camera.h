fe1854b9ec5110d5776552462059ea4aca265169
Jiahan Du <dujiahan@amazon.com>
2025-07-05 16:00:15 -0700

    m

--- include/Camera.h
+++ include/Camera.h
@@ -9,18 +9,20 @@ class Camera{
     int image_height; //Number of pixels per column
     double aspect_ratio=(double)image_width/image_height; 
     Point center(0,0,0);  //Location of the camera
-    Point top_left; // The top left corner of the view_port
+    Point top_left; // The bottom left corner of the view_port
 
-    void initialize(){
-      
-    }
-    
     Color ray_color(const Ray& r, const Object& world) const{
         
     }  
 
     void render(const Object& world){
+      std::cout<<"P3\n"<<image_width<<' '<<image_height<<"\n255\n";
 
+      for(int i=0;i<height;i++){
+        for(int j=0;j<height;j++){
+          
+        }
+      }
     }
 };
 
